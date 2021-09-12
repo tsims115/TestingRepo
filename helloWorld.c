959e129aea85162b7496b779a3f5f62a58f4527a
tsims115 <tsims115@yahoo.com>
2021-09-12 21:10:04 +0000

    made a for loop that prints hello world 1000 times!

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    printf("Hello World!\n");
+    char name[50];
+    printf("What is your name young one?\n");
+    scanf("%s", name);
+    printf("Nice to meet you %s\nNow starting for loop!\n", name);
+    for(int i = 0; i < 1000; i++){
+      printf("Hello World!\n");
+    }
     return 0;
 }
